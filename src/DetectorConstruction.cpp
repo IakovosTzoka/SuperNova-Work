bcd339389707bdc12dacfa92ba94df751fa1aac9
IakovosTzoka <iakovos.tzoka@uta.edu>
2026-06-04 13:56:47 -0400

    Update detector optical boundary construction

--- src/DetectorConstruction.cpp
+++ src/DetectorConstruction.cpp
@@ -21,7 +21,7 @@
 #include "CLHEP/Units/SystemOfUnits.h"
 #include "G4OpticalSurface.hh"
 #include "../cfg/config.h"
-#include "G4LogicalSkinSurface.hh"
+#include "G4LogicalBorderSurface.hh"
 #ifdef With_Opticks
 #include "G4CXOpticks.hh"
 #endif
@@ -97,15 +97,17 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
 
   G4ThreeVector offset(ConfigManager::GetDetectorWidth()/2., ConfigManager::GetDetectorHeight()/2., ConfigManager::GetDetectorLength()/2.);
 
-  new G4PVPlacement(0, offset,
-                    detector_logic_vol, "detector.physical", world_logic_vol, false, 0, true);
+  G4VPhysicalVolume* detector_phys_vol =
+    new G4PVPlacement(0, offset,
+                      detector_logic_vol, "detector.physical", world_logic_vol, false, 0, true);
 
-  new G4PVPlacement(0, G4ThreeVector(0,0.,325*CLHEP::cm/2),
-                    Photon_detector_logic_vol, "Photon_detector.physical", detector_logic_vol, false, 0, true);
+  G4VPhysicalVolume* photon_detector_phys_vol =
+    new G4PVPlacement(0, G4ThreeVector(0,0.,178*CLHEP::cm),
+                      Photon_detector_logic_vol, "Photon_detector.physical", detector_logic_vol, false, 0, true);
   //////////////////////////////////////////////////////////
-G4OpticalSurface * ops= new G4OpticalSurface("SiliconeDetector",unified,polished,dielectric_metal);
-ops->SetMaterialPropertiesTable(OpticalMaterialProperties::PerfectDetector());
-G4LogicalSkinSurface * lss = new G4LogicalSkinSurface("SiliconeDetector",detector_logic_vol,ops);
+	G4OpticalSurface * ops= new G4OpticalSurface("SiliconeDetector",unified,polished,dielectric_metal);
+	ops->SetMaterialPropertiesTable(OpticalMaterialProperties::PerfectDetector());
+		new G4LogicalBorderSurface("SiliconeDetector", detector_phys_vol, photon_detector_phys_vol, ops);
 
 
 #ifdef With_Opticks
